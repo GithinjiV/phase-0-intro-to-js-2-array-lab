5cc5a00534306394ca9c69726a598ac1cd640c27
vinnie <githinjigicheru146@gmail.com>
2024-01-24 23:31:28 +0300

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name){
+    const newCats =[name, ...cats];
+    return newCats;
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
+
+function removeLastCat(){
+    return cats.slice(0,cats.length - 1);
+}
+
